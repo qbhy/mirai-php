6ba372223aecada104002a974b1288569174f4c4
qbhy <96qbhy@gmail.com>
2020-07-28 10:07:55 +0800

    添加戳一戳类型

--- src/Message/Poke.php
+++ src/Message/Poke.php
@@ -6,6 +6,13 @@ class Poke extends AbstractMessage
 {
     protected $type = MessageTypeConst::POKE;
 
+    const POKE = 'Poke'; // 戳一戳
+    const SHOW_LOVE = 'ShowLove'; // 比心
+    const LIKE = 'Like'; // 点赞
+    const HEART_BROKEN = 'ShowLove'; // 心碎
+    const SIX_SIX_SIX = 'SixSixSix'; // 666
+    const FANG_DA_ZHAO = 'FangDaZhao'; // 放大招
+
     public function name()
     {
         return $this->getRawValue('name');
